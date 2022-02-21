266ae69b2d922ea1be00cfc751eae5ab44337325
zeinaelayoubi <99473167+zeinaelayoubi@users.noreply.github.com>
2022-02-21 18:06:07 +0200

    Create Employee.java

---
+++ staffManagment/Employee.java
@@ -0,0 +1,14 @@
+public class Employee {
+private String FirstName;
+private String LastName;
+private int Age;
+private double MonthlySalary;
+public void Employee(String firstName, String lastname, int age,double salary) {
+	 this.FirstName=firstName;
+	 this.LastName=lastname;
+	 this.Age=age;
+	 this.MonthlySalary=salary;
+	 
+}
+ 
+}
